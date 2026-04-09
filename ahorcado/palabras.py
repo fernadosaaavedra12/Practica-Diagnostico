4686aaa1f855d2a395074f36d4edd79535b896f0
Fernando Saavedra <saavedravargas624@gmail.com>
2026-04-08 20:13:12 -0400

    FernandoSaavedra: Eliminando carpetas de ahorcado y microevaluacion del repositorio

--- ahorcado/palabras.py
+++
@@ -1,8 +0,0 @@
-# palabras.py
-LISTA_PALABRAS = {
-    "python": "Lenguaje de programación muy popular.",
-    "terminal": "Interfaz de línea de comandos donde estás ejecutando esto.",
-    "linux": "Sistema operativo de código abierto, del pingüino.",
-    "ciberseguridad": "Práctica de proteger sistemas, redes y programas.",
-    "algoritmo": "Conjunto de instrucciones paso a paso para resolver un problema."
-}
