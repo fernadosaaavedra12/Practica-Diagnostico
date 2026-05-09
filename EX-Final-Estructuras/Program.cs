2dbba2bd056b9870c914cae06ee16b148ea0b1c4
Fernando Saavedra <saavedravargas624@gmail.com>
2026-05-08 20:20:28 -0400

    limpieza de archivos

--- EX-Final-Estructuras/Program.cs
+++ EX-Final-Estructuras/Program.cs
@@ -1,14 +1,5 @@
 using EX_Final_Estructuras.Services;
 
-namespace EX_Final_Estructuras
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            NetworkSimulator simulator = new NetworkSimulator();
-
-            simulator.Simulate();
-        }
-    }
-}
+// Instanciamos el servicio y ejecutamos el motor de simulación
+var simulator = new NetworkSimulator();
+simulator.Run();
